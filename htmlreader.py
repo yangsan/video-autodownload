62ccc6277e855e9e40b77e87050999492908ed30
Kevin Young <kevinyounglogin@163.com>
2014-05-15 22:35:01 +0800

    已经可以解析eztv.it了，直接要磁力链，很方便

--- htmlreader.py
+++ htmlreader.py
@@ -1,23 +1,42 @@
 # -*- coding: utf8 -*-
 from bs4 import BeautifulSoup
 import urllib2
-import urllib
-import cookielib
+#import urllib
+#import cookielib
 import sys
 
-#url = "http://c.learncodethehardway.org/book/"
-#url = "http://www.ttmeiju.com/"
-url = "http://www.ttmeiju.com/meiju/Late.Night.with.Seth.Meyers.html"
 
-req = urllib2.Request(url, headers={'User-Agent': "Magic Browser"})
+def correct_encoding(html):
+    """
+    check the html's encoding and return in utf-8
+    """
+    #soup = BeautifulSoup(html)
+    return html
 
-resp = urllib2.urlopen(req)
 
-respHTML = resp.read()
+if __name__ == "__main__":
 
-code = sys.getfilesystemencoding()
+    #url = "http://c.learncodethehardway.org/book/"
+    #ur l = "http://www.ttmeiju.com/"
+    #url = "http://www.ttmeiju.com/meiju/Late.Night.with.Seth.Meyers.html"
+    url = "http://eztv.it/shows/991/seth-meyers-late-night-with/"
 
-with open('ttmeiju.html','w') as f:
-    f.write(respHTML.decode(encoding="gbk").encode(code))
-print type(respHTML)
-#print respHTML
+    req = urllib2.Request(url, headers={'User-Agent': "Magic Browser"})
+
+    resp = urllib2.urlopen(req)
+
+    code = sys.getfilesystemencoding()
+
+    respHTML = resp.read().decode(encoding="gbk").encode(code)
+
+    soup = BeautifulSoup(respHTML)
+
+    for item in soup.find_all("a", class_='magnet'):
+        print item
+
+
+
+    #with open('sethmeyers.html','w') as f:
+        #f.write(respHTML.decode(encoding="gbk").encode(code))
+        #f.write(respHTML)
+    #print respHTML
