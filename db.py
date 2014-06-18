3c867c6f6c26eb9692828236408b91fb99290085
Kevin Young <kevinyounglogin@163.com>
2014-06-18 21:29:18 +0800

    完成了展示节目单和添加节目单的部分

--- db.py
+++ db.py
@@ -1,24 +1,106 @@
 # -*- coding: utf8 -*-
 import sqlite3
 
-#initilize or connect the database
-conn = sqlite3.connect("showlist.db")
-cursor = conn.cursor()
+
+def _connectDatabase():
+    """
+    Initialize or connect the database
+    """
+    conn = sqlite3.connect("showlist.db")
+    cursor = conn.cursor()
+    return conn, cursor
 
 
-try:
-    cursor.execute("select * from showlist")
-except StandardError, e:
-    print e, "\n try to create one."
+def _closeDatabase(conn, cursor):
+    """
+    Commit the change and close the cursor conn
+    """
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+def getShowList():
+    """
+    get show list from table showlist
+    """
+    conn, cursor = _connectDatabase()
+
+    try:
+        cursor.execute("select * from showlist")
+    except sqlite3.Error, e:
+        createShowListTable()
+        cursor.execute("select * from showlist")
+
+    show_list = cursor.fetchall()
+    _closeDatabase(conn, cursor)
+    return show_list
+
+def addNewRowInShowTable(show_name, url, show_table_name):
+    """
+    add a new show into the show table
+    """
+    conn, cursor = _connectDatabase()
+
+    #try:
     cursor.execute("""
-                   create table showlist
-                   (
-                   showname text,
-                   url text,
-                   tablename text
-                   )
-                   """)
-    cursor.execute("select * from showlist")
+                    insert into showlist
+                    (showname, url, tablename)
+                    values
+                    (?, ?, ?)
+                    """,
+                    (show_name, url, show_table_name))
+
+    #except sqlite3.OperationalError, e:
+        #createShowListTable(cursor)
+        #cursor.execute("""
+                       #insert into showlist
+                       #(showname, url, tablename)
+                       #values
+                       #(?, ?, ?)
+                       #)
+                       #""",
+                       #(show_name, url, show_table_name))
+
+    _closeDatabase(conn, cursor)
+
+
+
+def createShowListTable(cursor):
+    cursor.execute("""
+                create table showlist
+                (
+                showname text,
+                url text,
+                tablename text
+                )
+                """)
+
+def createEpListTable():
+    pass
+
+def deleteTable():
+    pass
+
+def deleteRow():
+    pass
+
+def getTableContent():
+    pass
+
+
+#try:
+    #cursor.execute("select * from showlist")
+#except StandardError, e:
+    #print e, "\n try to create one."
+    #cursor.execute("""
+                   #create table showlist
+                   #(
+                   #showname text,
+                   #url text,
+                   #tablename text
+                   #)
+                   #""")
+    #cursor.execute("select * from showlist")
 
 #cursor.execute("""
                #create table person
@@ -40,9 +122,9 @@ except StandardError, e:
                #""")
 
 
-cursor.execute("select * from showlist")
-print cursor.fetchone()
-print type(cursor.fetchone())
+#cursor.execute("select * from showlist")
+#print cursor.fetchone()
+#print type(cursor.fetchone())
 #for row in cursor.execute("select * from user"):
     #print type(row)
     #print row
@@ -58,6 +140,3 @@ print type(cursor.fetchone())
 #cursor.execute("drop table person")
 
 
-conn.commit()
-cursor.close()
-conn.close()
--- videodownloader.py
+++ videodownloader.py
@@ -1,2 +1,68 @@
 # -*- coding: utf8 -*-
+from bs4 import BeautifulSoup
+import urllib2
+import db
 
+
+#url reader
+#########################################################################
+def htmlReader(url):
+    """
+    Taking in a url return a BeautifulSoup soup object.
+    """
+    req = urllib2.Request(url, headers={'User-Agent': "Magic Browser"})
+    resp = urllib2.urlopen(req)
+    respHTML = resp.read()
+
+    return BeautifulSoup(respHTML)
+
+
+#show list maintainer
+#######################################################################e#
+def showShowList():
+    show_list = db.getShowList()
+    if show_list:
+        for number, show in enumerate(show_list):
+            print "%d) %s" % (number, show[0])
+    else:
+        print "The show list is empty, please add some."
+
+def addNewShow():
+    show_name = raw_input("Enter the show name:")
+    url = raw_input("Enter show url:")
+    show_table_name = show_name.replace(" ", "_")
+    db.addNewRowInShowTable(show_name, url, show_table_name)
+
+
+def deleteShow():
+    pass
+
+
+#swich func
+#########################################################################
+def switch(flag):
+    return {
+        "1": showShowList,
+        "2": addNewShow
+    }[flag]
+
+
+if __name__ == "__main__":
+
+    while True:
+        print "_" * 60
+        print "*" * 60
+        print "What do you want?"
+        print "1.Show show list"
+        print "2.Add new show"
+        flag = raw_input("==>")
+        switch(flag)()
+
+    #url = "http://www.ttmeiju.com/meiju/The.Tonight.Show.Starring.Jimmy.Fallon.html"
+
+    #soup = htmlReader(url)
+
+    #for item in soup.find_all("a", title=u"磁力链"):
+        ##print type(item)
+        #print item.parent.previous_sibling.previous_sibling
+        #break
